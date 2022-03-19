b37fbb565ddc4c4e12e5b52b49257394befcbb18
terapotikare <mervekomecc@gmail.com>
2022-03-19 09:47:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-x9xali
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-x9xali)
---
+++ src/language.js
@@ -0,0 +1,11 @@
+const tr={
+  settings:"Tweet Ayarları"
+};
+const en={
+  settings:"Tweet Settings"
+};
+const language={
+  tr,
+  en
+}
+export {language}
